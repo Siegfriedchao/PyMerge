b898b26a5bbea6d20a098481dab523f1f5b19f58
Youchao Wang <wang_youchao1234@126.com>
2019-12-14 14:11:23 +0000

    Update PyMerge.py

--- PyMerge.py
+++ PyMerge.py
@@ -1,3 +1,21 @@
 import math
+import glob
 from PIL import Image
 
+
+# Read all the images
+image_list = []
+for filename in glob.glob('test/*.tiff'): #assuming tiff
+    im=Image.open(filename)
+    image_list.append(im)
+
+unitWidth, unitHeight = im.size
+
+# debug use
+print(unitWidth, unitHeight)
+
+im = image_list[3]
+im.show()
+
+# im = Image.open('images/MDPC501_191112140001_A01f00d0.TIFF')
+# im.show()
