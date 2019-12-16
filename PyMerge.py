965dbf7bba7ef319fc3c443975804c72454dcb5a
Youchao Wang <wang_youchao1234@126.com>
2019-12-16 15:45:52 +0000

    Cleanup

--- PyMerge.py
+++ PyMerge.py
@@ -10,7 +10,7 @@ import numpy as np
 import cv2 as cv
 
 # Please set the following parameters
-PATH = 'images/'
+PATH = 'test/'
 OUTPUT_PATH = 'output/'
 PICTURE_NUMBER = 'A01'
 PICTURE_CHANNEL = 'd0'
@@ -51,10 +51,7 @@ def read_image(PATH):
 
 	return imageList
 
-def process_image(imageList):
-	# Build up a concat map, assuming a sqaure layout
-	# This is for subimages without perfect square
-	totalSize = len(imageList) + 4
+def process_image_map(totalSize, isClockwise):
 	rowCol = int(math.sqrt(totalSize))
 	centre = int(rowCol // 2)
 	# print(centre)
@@ -63,7 +60,10 @@ def process_image(imageList):
 	coordinateList = [[None] * rowCol for i in range(rowCol)]
 
 	direction = 'l'
-	left, down, right, up = 1, 1, 2, 2
+	if isClockwise:
+		left, up, right, down = 1, 1, 2, 2
+	else:
+		left, down, right, up = 1, 1, 2, 2
 	tmp = 0
 	j = 0
 
@@ -83,45 +83,48 @@ def process_image(imageList):
 			X = X - 1
 			tmp += 1
 			if (tmp == left):
-				direction = 'd'
+				if isClockwise:
+					direction = 'u'
+				else:
+					direction = 'd'
 				left = left + 2
 				tmp = 0
 		elif direction == 'u':
 			Y = Y - 1
 			tmp += 1
 			if (tmp == up):
-				direction = 'l'
+				if isClockwise:
+					direction = 'r'
+				else:
+					direction = 'l'
 				up = up + 2
 				tmp = 0
 		elif direction == 'r':
 			X = X + 1
 			tmp += 1
 			if (tmp == right):
-				direction = 'u'
+				if isClockwise:
+					direction = 'd'
+				else:
+					direction = 'u'
 				right = right + 2
 				tmp = 0
 		elif direction == 'd':
 			Y = Y + 1
 			tmp += 1
 			if (tmp == down):
-				direction = 'r'
+				if isClockwise:
+					direction = 'l'
+				else:
+					direction = 'r'
 				down = down + 2
 				tmp = 0
 
 	coordinateList[Y][X] = 999 # make sure the last corner has been accounted
 
-	print('Subimages in the order of')
-	for i in coordinateList:
-		for tmp in i:
-			print('%2d ' % tmp, end = '')
-		print()
-
 	return coordinateList
 
-def process_image_without_corner(imageList):
-	# Build up a concat map, assuming a sqaure layout
-	# This is for 4, 9, 16... subimages
-	totalSize = len(imageList)
+def process_image_map_without_corner(totalSize, isClockwise):
 	rowCol = int(math.sqrt(totalSize))
 	centre = int(rowCol // 2)
 	# print(centre)
@@ -130,7 +133,10 @@ def process_image_without_corner(imageList):
 	coordinateList = [[None] * rowCol for i in range(rowCol)]
 
 	direction = 'l'
-	left, down, right, up = 1, 1, 2, 2
+	if isClockwise:
+		left, up, right, down = 1, 1, 2, 2
+	else:
+		left, down, right, up = 1, 1, 2, 2
 	tmp = 0
 	j = 0
 
@@ -145,32 +151,70 @@ def process_image_without_corner(imageList):
 			X = X - 1
 			tmp += 1
 			if (tmp == left):
-				direction = 'u'
+				if isClockwise:
+					direction = 'u'
+				else:
+					direction = 'd'
 				left = left + 2
 				tmp = 0
 		elif direction == 'u':
 			Y = Y - 1
 			tmp += 1
 			if (tmp == up):
-				direction = 'r'
+				if isClockwise:
+					direction = 'r'
+				else:
+					direction = 'l'
 				up = up + 2
 				tmp = 0
 		elif direction == 'r':
 			X = X + 1
 			tmp += 1
 			if (tmp == right):
-				direction = 'd'
+				if isClockwise:
+					direction = 'd'
+				else:
+					direction = 'u'
 				right = right + 2
 				tmp = 0
 		elif direction == 'd':
 			Y = Y + 1
 			tmp += 1
 			if (tmp == down):
-				direction = 'l'
+				if isClockwise:
+					direction = 'l'
+				else:
+					direction = 'r'
 				down = down + 2
 				tmp = 0
 
 	coordinateList[Y][X] = totalSize - 1 # make sure the last corner has been accounted
+
+	return coordinateList
+
+def process_image(imageList):
+	# Build up a concat map, assuming a sqaure layout
+	# This is for subimages without perfect square
+	totalSize = len(imageList) + 4
+	isClockwise = 0
+
+	coordinateList = process_image_map(totalSize, isClockwise)
+
+	print('Subimages in the order of')
+	for i in coordinateList:
+		for tmp in i:
+			print('%2d ' % tmp, end = '')
+		print()
+
+	return coordinateList
+
+def process_image_without_corner(imageList):
+	# Build up a concat map, assuming a sqaure layout
+	# This is for 4, 9, 16... subimages
+	totalSize = len(imageList)
+	isClockwise = 0
+
+	coordinateList = process_image_map_without_corner(totalSize, isClockwise)
 
 	print('Subimages in the order of')
 	for i in coordinateList:
@@ -185,60 +229,9 @@ def process_image_clockwise(imageList):
 	# This is for subimages without perfect square
 	# Clock-wise
 	totalSize = len(imageList) + 4
-	rowCol = int(math.sqrt(totalSize))
-	centre = int(rowCol // 2)
-	# print(centre)
-	X = centre
-	Y = centre
-	coordinateList = [[None] * rowCol for i in range(rowCol)]
+	isClockwise = 1
 
-	direction = 'l'
-	left, up, right, down = 1, 1, 2, 2
-	tmp = 0
-	j = 0
-
-	for i in range(1, totalSize):
-		if (X == 0 and Y == 0) or (X == 0 and Y == (rowCol - 1) ) or (X == (rowCol - 1) and Y == (rowCol - 1) ) or (X == (rowCol - 1) and Y == 0 ):
-			j = j - 1
-			coordinateList[Y][X] = 999 # this is a magic number, better use enum
-		else:
-			coordinateList[Y][X] = j
-		
-		j = j + 1
-		# print(X, Y)
-
-		# Algorithm to generate mapping
-		# l for left, d for down, r for right, u for up
-		if direction == 'l':
-			X = X - 1
-			tmp += 1
-			if (tmp == left):
-				direction = 'u'
-				left = left + 2
-				tmp = 0
-		elif direction == 'u':
-			Y = Y - 1
-			tmp += 1
-			if (tmp == up):
-				direction = 'r'
-				up = up + 2
-				tmp = 0
-		elif direction == 'r':
-			X = X + 1
-			tmp += 1
-			if (tmp == right):
-				direction = 'd'
-				right = right + 2
-				tmp = 0
-		elif direction == 'd':
-			Y = Y + 1
-			tmp += 1
-			if (tmp == down):
-				direction = 'l'
-				down = down + 2
-				tmp = 0
-
-	coordinateList[Y][X] = 999 # make sure the last corner has been accounted
+	coordinateList = process_image_map(totalSize, isClockwise)
 
 	print('Subimages in the order of')
 	for i in coordinateList:
@@ -253,55 +246,9 @@ def process_image_without_corner_clockwise(imageList):
 	# This is for 4, 9, 16... subimages
 	# Clock-wise
 	totalSize = len(imageList)
-	rowCol = int(math.sqrt(totalSize))
-	centre = int(rowCol // 2)
-	# print(centre)
-	X = centre
-	Y = centre
-	coordinateList = [[None] * rowCol for i in range(rowCol)]
+	isClockwise = 1
 
-	direction = 'l'
-	left, down, right, up = 1, 1, 2, 2
-	tmp = 0
-	j = 0
-
-	for i in range(1, totalSize):
-		
-		coordinateList[Y][X] = i - 1
-		# print(X, Y)
-
-		# Algorithm to generate mapping
-		# l for left, d for down, r for right, u for up
-		if direction == 'l':
-			X = X - 1
-			tmp += 1
-			if (tmp == left):
-				direction = 'd'
-				left = left + 2
-				tmp = 0
-		elif direction == 'u':
-			Y = Y - 1
-			tmp += 1
-			if (tmp == up):
-				direction = 'l'
-				up = up + 2
-				tmp = 0
-		elif direction == 'r':
-			X = X + 1
-			tmp += 1
-			if (tmp == right):
-				direction = 'u'
-				right = right + 2
-				tmp = 0
-		elif direction == 'd':
-			Y = Y + 1
-			tmp += 1
-			if (tmp == down):
-				direction = 'r'
-				down = down + 2
-				tmp = 0
-
-	coordinateList[Y][X] = totalSize - 1 # make sure the last corner has been accounted
+	coordinateList = process_image_map_without_corner(totalSize, isClockwise)
 
 	print('Subimages in the order of')
 	for i in coordinateList:
